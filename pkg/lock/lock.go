ebf62a057ede949eb8679976b5cbac8d1af250f6
陈颖鹏 <520icarus1314@gmail.com>
2023-10-08 11:22:04 +0800

    [style] 修改注释

--- pkg/lock/lock.go
+++ pkg/lock/lock.go
@@ -7,9 +7,13 @@ import (
 )
 
 type Lock interface {
+	// Lock put an element into channel to mark that locked
 	Lock()
+	// TryLock returns true if trying lock success else false
 	TryLock() bool
+	// TryLockWithTimeout returns true if trying lock success that within the specified time else false
 	TryLockWithTimeout(timeout time.Duration) bool
+	// Unlock panic if unlock of unlocked lock
 	Unlock()
 }
 
@@ -17,12 +21,10 @@ type lock struct {
 	lock chan struct{}
 }
 
-// Lock put an element into channel to mark that locked
 func (l *lock) Lock() {
 	l.lock <- struct{}{}
 }
 
-// TryLock returns true if trying lock success else false
 func (l *lock) TryLock() (locked bool) {
 	select {
 	case l.lock <- struct{}{}:
@@ -32,7 +34,6 @@ func (l *lock) TryLock() (locked bool) {
 	return
 }
 
-// TryLockWithTimeout returns true if trying lock success that within the specified time else false
 func (l *lock) TryLockWithTimeout(timeout time.Duration) (locked bool) {
 	timeoutCtx, cancelFunc := context.WithTimeout(context.TODO(), timeout)
 	defer cancelFunc()
@@ -44,7 +45,6 @@ func (l *lock) TryLockWithTimeout(timeout time.Duration) (locked bool) {
 	return
 }
 
-// Unlock panic if unlock of unlocked lock
 func (l *lock) Unlock() {
 	select {
 	case <-l.lock:
