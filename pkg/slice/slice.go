86123a6ed6a235799eeec5e1735a61fd310124c8
chenyingpeng <520icarus1314@gmail.com>
2024-01-29 15:45:46 +0800

    refactor(slice): 新增可以自定义判断条件判断元素是否在数组的方法

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -2,22 +2,17 @@ package slice
 
 // Exist determines whether the target is in the s
 func Exist[T comparable](s []T, target T) bool {
+	return CustomizeExist(s, target, func(s, target T) bool { return s == target })
+}
+
+// CustomizeExist use customize function to determines the target is in the s
+// customize: return true if s == target
+func CustomizeExist[T any](s []T, target T, customize func(s, target T) bool) bool {
 	for i := range s {
-		if s[i] == target {
+		if customize(s[i], target) {
 			return true
 		}
 	}
 
 	return false
-}
-
-// NotExist determines whether the target is not in the s
-func NotExist[T comparable](s []T, target T) bool {
-	for i := range s {
-		if s[i] == target {
-			return false
-		}
-	}
-
-	return true
 }
--- test/pkg/slice/slice_test.go
+++ test/pkg/slice/slice_test.go
@@ -9,18 +9,29 @@ import (
 
 func TestSlice_Exist(t *testing.T) {
 	ast := assert.New(t)
-
 	strList := []string{"a", "b", "c"}
 
+	// determines whether the target is in the s
 	ast.Equal(slice.Exist(strList, "a"), true, "slice.Exist must return true value")
 	ast.Equal(slice.Exist(strList, "d"), false, "slice.Exist must return false value")
+	// determines whether the target is not in the s
+	ast.Equal(!slice.Exist(strList, "a"), false, "slice.Exist must return false value")
+	ast.Equal(!slice.Exist(strList, "d"), true, "slice.Exist must return true value")
 }
 
-func TestSlice_NotExist(t *testing.T) {
+func TestSlice_CustomizeExist(t *testing.T) {
 	ast := assert.New(t)
+	type TestModel struct{ Latter string }
+	strList := []TestModel{{Latter: "a"}, {Latter: "b"}, {Latter: "c"}}
 
-	strList := []string{"a", "b", "c"}
-
-	ast.Equal(slice.NotExist(strList, "a"), false, "slice.Exist must return false value")
-	ast.Equal(slice.NotExist(strList, "d"), true, "slice.Exist must return true value")
+	// determines whether the target is in the s
+	ast.Equal(slice.CustomizeExist(strList, TestModel{Latter: "a"},
+		func(s, target TestModel) bool { return s.Latter == target.Latter }), true, "slice.Exist must return true value")
+	ast.Equal(slice.CustomizeExist(strList, TestModel{Latter: "d"},
+		func(s, target TestModel) bool { return s.Latter == target.Latter }), false, "slice.Exist must return false value")
+	// determines whether the target is not in the s
+	ast.Equal(!slice.CustomizeExist(strList, TestModel{Latter: "a"},
+		func(s, target TestModel) bool { return s.Latter == target.Latter }), false, "slice.Exist must return false value")
+	ast.Equal(!slice.CustomizeExist(strList, TestModel{Latter: "d"},
+		func(s, target TestModel) bool { return s.Latter == target.Latter }), true, "slice.Exist must return true value")
 }
