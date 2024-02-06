a7997c45b006e4718aeb42f415839469aba74340
chenyingpeng <520icarus1314@gmail.com>
2024-02-06 10:25:14 +0800

    feat(slice): 数组操作(并、交、差、补)

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -2,17 +2,90 @@ package slice
 
 // Exist determines whether the target is in the s
 func Exist[T comparable](s []T, target T) bool {
-	return CustomizeExist(s, target, func(s, target T) bool { return s == target })
-}
-
-// CustomizeExist use customize function to determines the target is in the s
-// customize: return true if s == target
-func CustomizeExist[T any](s []T, target T, customize func(s, target T) bool) bool {
 	for i := range s {
-		if customize(s[i], target) {
+		if s[i] == target {
 			return true
 		}
 	}
 
 	return false
+}
+
+// Distinct make the element is unique in the slice
+func Distinct[T comparable](slice []T) (final []T) {
+	m := make(map[T]struct{})
+	for _, e := range slice {
+		if _, ok := m[e]; !ok {
+			m[e] = struct{}{}
+			final = append(final, e)
+		}
+	}
+
+	return
+}
+
+// Union return the elements that exist in sliceA or sliceB
+func Union[T comparable](sliceA, sliceB []T) []T {
+	return Distinct(append(sliceA, sliceB...))
+}
+
+// Intersect return the elements that exist in both sliceA and sliceB
+func Intersect[T comparable](sliceA, sliceB []T) (final []T) {
+	m := make(map[T]struct{})
+	for _, a := range sliceA {
+		if _, ok := m[a]; !ok {
+			m[a] = struct{}{}
+		}
+	}
+
+	for _, b := range sliceB {
+		if _, ok := m[b]; ok {
+			final = append(final, b)
+		}
+	}
+
+	return
+}
+
+// Subtract return the elements that exist in sliceIn but not exist in sliceNotIn
+func Subtract[T comparable](sliceIn, sliceNotIn []T) (final []T) {
+	mapNotIn := make(map[T]struct{})
+	for _, notIn := range sliceNotIn {
+		if _, ok := mapNotIn[notIn]; !ok {
+			mapNotIn[notIn] = struct{}{}
+		}
+	}
+
+	for _, in := range sliceIn {
+		if _, ok := mapNotIn[in]; !ok {
+			final = append(final, in)
+		}
+	}
+
+	return
+}
+
+// ExclusiveOr return the elements that exist in sliceA but not in sliceB or in sliceB but not in sliceA
+func ExclusiveOr[T comparable](sliceA, sliceB []T) (final []T) {
+	mapA := make(map[T]struct{})
+	mapB := make(map[T]struct{})
+
+	for _, b := range sliceB {
+		mapB[b] = struct{}{}
+	}
+
+	for _, a := range sliceA {
+		mapA[a] = struct{}{}
+		if _, ok := mapB[a]; !ok {
+			final = append(final, a)
+		}
+	}
+
+	for b := range mapB {
+		if _, ok := mapA[b]; !ok {
+			final = append(final, b)
+		}
+	}
+
+	return
 }
--- test/pkg/slice/slice_test.go
+++ test/pkg/slice/slice_test.go
@@ -19,19 +19,64 @@ func TestSlice_Exist(t *testing.T) {
 	ast.Equal(!slice.Exist(strList, "d"), true, "slice.Exist must return true value")
 }
 
-func TestSlice_CustomizeExist(t *testing.T) {
+func TestSlice_Distinct(t *testing.T) {
 	ast := assert.New(t)
-	type TestModel struct{ Latter string }
-	strList := []TestModel{{Latter: "a"}, {Latter: "b"}, {Latter: "c"}}
+	strList := []string{"b", "a", "b", "c", "c", "a"}
 
-	// determines whether the target is in the s
-	ast.Equal(slice.CustomizeExist(strList, TestModel{Latter: "a"},
-		func(s, target TestModel) bool { return s.Latter == target.Latter }), true, "slice.Exist must return true value")
-	ast.Equal(slice.CustomizeExist(strList, TestModel{Latter: "d"},
-		func(s, target TestModel) bool { return s.Latter == target.Latter }), false, "slice.Exist must return false value")
-	// determines whether the target is not in the s
-	ast.Equal(!slice.CustomizeExist(strList, TestModel{Latter: "a"},
-		func(s, target TestModel) bool { return s.Latter == target.Latter }), false, "slice.Exist must return false value")
-	ast.Equal(!slice.CustomizeExist(strList, TestModel{Latter: "d"},
-		func(s, target TestModel) bool { return s.Latter == target.Latter }), true, "slice.Exist must return true value")
+	distinct := slice.Distinct(strList)
+	ast.Equal("b", distinct[0])
+	ast.Equal("a", distinct[1])
+	ast.Equal("c", distinct[2])
+}
+
+func TestSlice_Union(t *testing.T) {
+	ast := assert.New(t)
+	sliceA := []string{"a", "b"}
+	sliceB := []string{"c", "d"}
+
+	union := slice.Union(sliceA, sliceB)
+	ast.Equal(4, len(union))
+	if len(union) == 4 {
+		ast.Equal("a", union[0])
+		ast.Equal("b", union[1])
+		ast.Equal("c", union[2])
+		ast.Equal("d", union[3])
+	}
+}
+
+func TestSlice_Intersect(t *testing.T) {
+	ast := assert.New(t)
+	sliceA := []string{"a", "b"}
+	sliceB := []string{"b", "c"}
+
+	intersect := slice.Intersect(sliceA, sliceB)
+	ast.Equal(1, len(intersect))
+	if len(intersect) == 1 {
+		ast.Equal("b", intersect[0])
+	}
+}
+
+func TestSlice_Subtract(t *testing.T) {
+	ast := assert.New(t)
+	sliceA := []string{"a", "b"}
+	sliceB := []string{"b", "c"}
+
+	subtract := slice.Subtract(sliceA, sliceB)
+	ast.Equal(1, len(subtract))
+	if len(subtract) == 1 {
+		ast.Equal("a", subtract[0])
+	}
+}
+
+func TestSlice_ExclusiveOr(t *testing.T) {
+	ast := assert.New(t)
+	sliceA := []string{"a", "b"}
+	sliceB := []string{"b", "c"}
+
+	subtract := slice.ExclusiveOr(sliceA, sliceB)
+	ast.Equal(2, len(subtract))
+	if len(subtract) == 2 {
+		ast.Equal("a", subtract[0])
+		ast.Equal("c", subtract[1])
+	}
 }
